8ba62d2bd07ce0294931c219de08c943d0f040ad
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-09-30 21:59:27 +0200

    feat: added fastfetch to hm

--- home/home.nix
+++ home/home.nix
@@ -140,6 +140,39 @@
 
   programs.fastfetch = {
     enable = true;
+    settings = {
+      modules = [
+        "title"
+        "separator"
+        "os"
+        "host"
+        "kernel"
+        "uptime"
+        "packages"
+        "shell"
+        # "display"
+        "de"
+        "wm"
+        "wmtheme"
+        # "theme"
+        # "icons"
+        # "font"
+        # "cursor"
+        "terminal"
+        # "terminalfont"
+        "cpu"
+        "gpu"
+        "memory"
+        # "swap"
+        # "disk"
+        # "localip"
+        # "battery"
+        # "poweradapter"
+        # "locale"
+        "break"
+        "colors"
+      ];
+    };
   };
 
   gtk = {
