e9bce35516362156847a8fd2a8980bcdb353ffc9
Simon Antonius Lauer <simon.lauer@posteo.de>
2025-04-08 19:09:12 +0200

    feat: simple rename

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS configuration";
+  description = "Nix configuration for my linux desktop and Mac";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
