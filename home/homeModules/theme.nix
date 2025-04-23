e0caf3ac78c2eb47ad16ccf1a169a5bc732e018c
Simon Antonius Lauer <simon.lauer@posteo.de>
2025-04-23 09:44:38 +0200

    feat: catppuccin mocha gtk

--- home/homeModules/theme.nix
+++ home/homeModules/theme.nix
@@ -1,11 +1,19 @@
 {pkgs, ...}: {
   gtk = {
     enable = true;
-    theme.name = "rose-pine-moon";
-    theme.package = pkgs.rose-pine-gtk-theme;
+    theme.package = pkgs.catppuccin-gtk.override {
+      variant = "mocha";
+    };
+    theme.name = "catppuccin-mocha-blue-standard";
     iconTheme = {
       package = pkgs.rose-pine-icon-theme;
       name = "rose-pine-moon";
     };
   };
 }
+# some other themes:
+# theme.name = "rose-pine-moon";
+# theme.package = pkgs.rose-pine-gtk-theme;
+# theme.name = "Nordic";
+# theme.package = pkgs.nordic;
+
