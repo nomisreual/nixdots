ced5b8a2bee6bd86a9eba3b8d31bcf543d4d2a01
Simon Antonius Lauer <simon.lauer@posteo.de>
2025-05-07 22:18:16 +0200

    chore: clean up

--- home/homeModules/waybar/waybar.nix
+++ home/homeModules/waybar/waybar.nix
@@ -36,7 +36,7 @@ in {
         ];
         idle_inhibitor = {
           format = "{icon}";
-          "format-icons" = {
+          format-icons = {
             activated = "";
             deactivated = "";
           };
@@ -46,8 +46,8 @@ in {
         };
         clock = {
           timezone = "Europe/Berlin";
-          "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-          "format-alt" = "{:%Y-%m-%d}";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          format-alt = "{:%Y-%m-%d}";
         };
         cpu = {
           format = "{usage}% ";
@@ -57,60 +57,60 @@ in {
           format = "{}% ";
         };
         temperature = {
-          "critical-threshold" = 80;
+          critical-threshold = 80;
           format = "{temperatureC}°C {icon}";
-          "format-icons" = ["" "" ""];
+          format-icons = ["" "" ""];
         };
         network = {
-          "format-wifi" = "{signalStrength}%  ";
-          "format-ethernet" = "{ipaddr}/{cidr}";
-          "tooltip-format" = "{ifname} via {gwaddr}";
-          "format-linked" = "{ifname} (No IP)";
-          "format-disconnected" = "Disconnected ⚠";
-          "format-alt" = "{ifname}: {ipaddr}/{cidr}";
+          format-wifi = "{signalStrength}%  ";
+          format-ethernet = "{ipaddr}/{cidr}";
+          tooltip-format = "{ifname} via {gwaddr}";
+          format-linked = "{ifname} (No IP)";
+          format-disconnected = "Disconnected ⚠";
+          format-alt = "{ifname}: {ipaddr}/{cidr}";
         };
         pulseaudio = {
-          "scroll-step" = 1;
-          "format" = "{volume}% {icon} {format_source}";
-          "format-bluetooth" = "{volume}% {icon} {format_source}";
-          "format-bluetooth-muted" = "󰝟 {icon} {format_source}";
-          "format-muted" = "󰝟 {format_source}";
-          "format-source" = " {volume}% ";
-          "format-source-muted" = " ";
-          "format-icons" = {
-            "headphone" = "";
-            "hands-free" = "󰋎";
-            "headset" = "󰋎";
-            "phone" = "";
-            "portable" = "";
-            "car" = "";
-            "default" = ["" "" ""];
+          scroll-step = 1;
+          format = "{volume}% {icon} {format_source}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-bluetooth-muted = "󰝟 {icon} {format_source}";
+          format-muted = "󰝟 {format_source}";
+          format-source = " {volume}% ";
+          format-source-muted = " ";
+          format-icons = {
+            headphone = "";
+            hands-free = "󰋎";
+            headset = "󰋎";
+            phone = "";
+            portable = "";
+            car = "";
+            default = ["" "" ""];
           };
-          "on-click" = "pavucontrol";
+          on-click = "pavucontrol";
         };
         "custom/media" = {
           format = "{icon} {}";
           escape = true;
-          "return-type" = "json";
-          "max-length" = 40;
-          "on-click" = "${playerctl} play-pause";
-          "on-click-right" = "${playerctl} stop";
-          "smooth-scrolling-threshold" = 10;
-          "on-scroll-up" = "${playerctl} next";
-          "on-scroll-down" = "${playerctl} previous";
+          return-type = "json";
+          max-length = 40;
+          on-click = "${playerctl} play-pause";
+          on-click-right = "${playerctl} stop";
+          smooth-scrolling-threshold = 10;
+          on-scroll-up = "${playerctl} next";
+          on-scroll-down = "${playerctl} previous";
           exec = "${mediaplayer}/bin/mediaplayer 2> /dev/null";
         };
         "hyprland-language" = {
-          "format" = "{}";
-          "format-en" = "en";
-          "format-de" = "de";
+          format = "{}";
+          format-en = "en";
+          format-de = "de";
         };
         "custom/power" = {
           format = "⏻ ";
           tooltip = false;
           menu = "on-click";
-          "menu-file" = ./power_menu.xml;
-          "menu-actions" = {
+          menu-file = ./power_menu.xml;
+          menu-actions = {
             shutdown = "shutdown 'now'";
             reboot = "reboot";
             suspend = "systemctl suspend";
