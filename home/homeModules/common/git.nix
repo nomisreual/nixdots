39d38c66ae9ca640871a09276c868ba6c376b4db
Simon Antonius Lauer <nomisreual@proton.me>
2025-05-10 17:36:04 +0200

    feat: adjust git settings, signing with gpg

--- home/homeModules/common/git.nix
+++ home/homeModules/common/git.nix
@@ -2,7 +2,12 @@
   programs.git = {
     enable = true;
     userName = "Simon Antonius Lauer";
-    userEmail = "simon.lauer@posteo.de";
+    userEmail = "nomisreual@proton.me";
+    signing = {
+      format = "openpgp";
+      key = "AFA7BD5B3FF3E367";
+      signByDefault = true;
+    };
     extraConfig = {
       init = {
         defaultbranch = "main";
