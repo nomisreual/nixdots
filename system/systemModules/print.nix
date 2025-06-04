b89aa2883629033f8b8fbe6169407a0b7083e63b
Simon Antonius Lauer <nomisreual@proton.me>
2025-06-04 10:19:48 +0200

    feat: declare printer on nixos

--- system/systemModules/print.nix
+++ system/systemModules/print.nix
@@ -8,4 +8,21 @@
     nssmdns4 = true;
     openFirewall = true;
   };
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "SamsungPrinter";
+        location = "Home";
+        deviceUri = "dnssd://Samsung%20M267x%20287x%20Series%20(SEC30CDA716A59A)._printer._tcp.local";
+        model = "gutenprint.5.3://pcl-g_6/expert";
+        ppdOptions = {
+          PageSize = "A4";
+          Duplex = "DuplexNoTumble";
+          Resolution = "600dpi";
+          ColorModel = "Gray";
+        };
+      }
+    ];
+    ensureDefaultPrinter = "SamsungPrinter";
+  };
 }
